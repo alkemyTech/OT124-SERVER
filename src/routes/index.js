535a0a148e9aacf7af7be812eadf69a6c7b33a9b
juanpa0011 <juanpablo1996epifani@gmail.com>
2022-01-25 17:57:03 -0300

    Add Route to Index.jS

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const activitiesRouter = require("./activities")
 
-router.use(('/activities'), activitiesRouter);
+router.use('/activities', activitiesRouter);
 
 //Importing the required routes here
 //Example: const userRouter = require("./routes/user");
