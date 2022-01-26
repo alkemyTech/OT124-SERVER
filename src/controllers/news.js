fd8487168c91360dce15544a266c8085183bbbf4
Diego Sosa <dhsosa98@gmail.com>
2022-01-26 15:32:39 -0300

    fixed update and delete news

--- src/controllers/news.js
+++ src/controllers/news.js
@@ -1,5 +1,38 @@
 const db = require('../models')
-const entity = 'News'
+const entity = 'entries'
+
+const updateNew = function(req, res, next) {
+    const {id} = req.params
+    const {name, content, image, categoryId, type } = req.body
+    const error = []
+
+    if (!name) {
+      error.push({text: "Agregar un nombre a la novedad"})
+    }
+    if (!content) {
+      error.push({text: "Agregar una breve descripcion a la novedad"})
+    }
+    if (!categoryId) {
+      error.push({text: "Agregar una id a la categoria"})
+    }
+
+    if (error.length > 0) {
+      res.send({ title: 'Novedades', messageErr: error })
+    } else {
+      db['News'].update(
+          { name, content, image, categoryId, type },
+          { where: { _id: id } }
+        )
+          .then(news =>
+              res.send({ title: 'Novedades', message: "Novedad actualizada" })
+          )
+          .error(err =>
+            console.log(err)
+          )
+
+    }
+
+};
 
 const deleteNew =  async function(req, res, next) {
     try{
@@ -17,10 +50,11 @@ const deleteNew =  async function(req, res, next) {
     catch(err){
       next(err)
     }
-  }
+  };
 
 const newsController = {
-    deleteNew
+    deleteNew,
+    updateNew
 };
 
 module.exports = newsController;
--- src/routes/news.js
+++ src/routes/news.js
@@ -1,42 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-const db = require('../models')
-//const New = require('../models/news')
+const newsController = require('../controllers/news')
 
-router.put('/news/:id', function(req, res, next) {
-    const {id} = req.params
-    const {name, content, image, categoryId, type } = req.body
-    const error = []
-
-    if (!name) {
-      error.push({text: "Agregar un nombre a la novedad"})
-    }
-    if (!content) {
-      error.push({text: "Agregar una breve descripcion a la novedad"})
-    }
-    if (!categoryId) {
-      error.push({text: "Agregar una id a la categoria"})
-    }
-
-    if (error.length > 0) {
-      res.send({ title: 'Novedades', messageErr: error })
-    } else {
-      db['News'].update(
-          { name, content, image, categoryId, type },
-          { where: { _id: id } }
-        )
-          .then(news =>
-              res.send({ title: 'Novedades', message: "Novedad actualizada" })
-          )
-          .error(err =>
-            console.log(err)
-          )
-
-    }
-
-});
-
+/* UPDATE new by ID. */
+router.put('/news/:id', newsController.updateNew);
 /* DELETE new by ID. */
 router.delete('/:id', newsController.deleteNew);
 
