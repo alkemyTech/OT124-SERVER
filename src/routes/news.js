2c9ae6d0a2dacfe1fb444db229b617b55f49513d
Francisco Rios <panchorios_242@hotmail.com>
2022-01-26 15:57:12 -0300

    Fix: path typo in news route

--- src/routes/news.js
+++ src/routes/news.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const newsController = require('../controllers/news')
 
 /* UPDATE new by ID. */
-router.put('/news/:id', newsController.updateNew);
+router.put('/:id', newsController.updateNew);
 /* DELETE new by ID. */
 router.delete('/:id', newsController.deleteNew);
 
