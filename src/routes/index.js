fcbfd2c85533b967dcf2ec02e40da4f46f3cf65c
Francisco Ríos Cabello <panchorios_242@hotmail.com>
2022-01-27 16:33:48 -0300

    Added: Created endpoint for listing all Contacts - OT124-73 (#14)
    * Added: model & migration of members
    
    * Added: Get all contacts endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const newRouter = require('./news');
 const activitiesRouter = require('./activities')
 const testimonialsRouter = require("./testimonials")
+const contactsRouter = require("./contacts")
 
 router.use('/news', newRouter)
 router.use('/testimonials', testimonialsRouter);
+router.use('/contacts', contactsRouter);
 router.use('/activities', activitiesRouter);
 
 //Importing the required routes here
