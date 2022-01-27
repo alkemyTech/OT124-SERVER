32c7741011693f054fe409c524bf80cf615223dd
necogamy <madrazonecogamy@gmail.com>
2022-01-27 18:07:32 -0300

    Project requeriments done

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,13 @@ const newRouter = require('./news');
 const activitiesRouter = require('./activities')
 const testimonialsRouter = require("./testimonials")
 const contactsRouter = require("./contacts")
+const organizationsRouter = require('./organizations');
 
 router.use('/news', newRouter)
 router.use('/testimonials', testimonialsRouter);
 router.use('/contacts', contactsRouter);
 router.use('/activities', activitiesRouter);
+router.use('/organizations', organizationsRouter);
 
 //Importing the required routes here
 //Example: const userRouter = require("./routes/user");
