afd6556a4b0b667016ad2c62a332e41d2109df52
Rafael Lynch <rafalynch.dev@gmail.com>
2022-01-27 16:43:47 -0300

    Minor fixes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const apiRouter = express.Router();
 const router = express.Router();
-
-//Importing the required routes here
-//Example: const userRouter = require("./routes/user");
 const newRouter = require("./news");
 const activitiesRouter = require("./activities");
 const testimonialsRouter = require("./testimonials");
+const contactsRouter = require("./contacts");
 
-//assingning the routes to the router
-// Example: router.use("/users", usersRouter);
 router.use("/news", newRouter);
-router.use("/activities", activitiesRouter);
 router.use("/testimonials", testimonialsRouter);
+router.use("/contacts", contactsRouter);
+router.use("/activities", activitiesRouter);
 
 apiRouter.use("/api/v1", router);
 
