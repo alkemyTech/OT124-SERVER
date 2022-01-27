9b4b23ee6dbd06faf9c929a3c4ec530b51aeed52
Franco Martin <francomartin.personal@gmail.com>
2022-01-27 20:51:28 -0300

    validator userDeleteSchema added

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const userController = require('../controllers/users')
+const userController = require('../controllers/users');
+const { validation } = require('../middlewares/validator');
+const { userDeleteSchema } = require('../validations/usersSchema');
 
 //delete a 'contact' or user
-router.delete('/:id', userController.deleteUser );
+router.delete('/:id',validation(userDeleteSchema), userController.deleteUser );
 
 module.exports = router;
