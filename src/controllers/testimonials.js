83694381575c4ee0941b6dcbcb66b18ed16c491d
Diego Sosa <dhsosa98@gmail.com>
2022-01-27 21:30:38 -0300

    added uploadImage service to create an aws object

--- src/controllers/testimonials.js
+++ src/controllers/testimonials.js
@@ -1,8 +1,11 @@
 const entity = 'Testimonials'
 const db = require('../models')
+const {uploadImage} = require('../services/fileUploader')
 
 const createTestimonial = async function(req, res, next) {
     try{
+    const {url} = await uploadImage(req.file, next) 
+    req.body.lastimage = url
     await db[entity].create(req.body)
     return res.status(200).send({
           errors: null,
