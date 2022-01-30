c72d7ca3a90146e0bff134b32696f5ca7f3aaccf
Nicolas-Marsan <nicomar80@gmail.com>
2022-01-30 12:39:05 -0300

    change controller/categories promises to async await form

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,34 +1,36 @@
-const db = require('../models')
-const entity = 'categories'
+const db = require("../models");
+const entity = "categories";
 
-const putCategories = async function(req, res, next) {
-
-    db.Categories.findAll({
-        where: {
-          id: { [Op.eq]: req.params.id },
-        },q
-      }).then(function (categories) {
-        if (!categories) {
-            db.Categories.update({
-                name: req.body.name,
-                description: req.body.description                
-              }, {
-                where: {
-                    id: req.params.id
-                }
-            });      
-              return res.json("Category created successfully");
-          
-        } else {       
-            return res.json("The category already exists");
+const putCategories = async function (req, res, next) {
+  try {
+    const update = await db[entity].findAll({
+      where: {
+        id: { [Op.eq]: req.params.id },
+      },
+    });
+    if (!update) {
+      db[entity].update(
+        {
+          name: req.body.name,
+          description: req.body.description,
+        },
+        {
+          where: {
+            id: req.params.id,
+          }
         }
-      });
-}
-
-
-const categoriesController = {
-    putCategories
-    
+      );
+      return res.json("Category created successfully");
+    } else {
+      return res.json("The category already exists");
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
-module.exports = categoriesController;
+const contactsController = {
+    putCategories,
+};
+
+module.exports = putCategories;
