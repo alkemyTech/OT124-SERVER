53884afb6cc118bd780534f5b3b61cf427fb68e5
Nicolás López <36177527+necogamy@users.noreply.github.com>
2022-01-31 18:21:33 -0300

    Ot124 17 (#18)
    * Fixed: package.json && scaffold project (#1)
    
    * Fixed: package.json && scaffold project
    
    * Added model & migration of entries and organization
    
    * package-lock.json updated
    
    * Project requeriments done
    
    Co-authored-by: Francisco Ríos Cabello <francisco.rios.cabello@gmail.com>
    Co-authored-by: Rafa Lynch <rafalynch.dev@gmail.com>
    Co-authored-by: Francisco Ríos Cabello <panchorios_242@hotmail.com>

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,25 +7,16 @@ const testimonialsRouter = require("./testimonials");
 const usersRouter = require('./users')
 const contactsRouter = require("./contacts");
 const filesRouter = require("./files")
+const organizationsRouter = require('./organizations');
 
 router.use("/news", newRouter);
 router.use("/testimonials", testimonialsRouter);
 router.use("/contacts", contactsRouter);
 router.use("/activities", activitiesRouter);
-router.use('/files', filesRouter);
 router.use("/users",usersRouter );
-//router.use("/mentiritas",mentiritasRouter );
-
-//Importing the required routes here
-//Example: const userRouter = require("./routes/user");
-
-//assingning the routes to the router
-// Example: router.use("/users", usersRouter);
-
-
-
 router.use('/files', filesRouter);
+router.use('/organizations', organizationsRouter);
+
 
 apiRouter.use("/api/v1", router);
-
 module.exports = apiRouter;
