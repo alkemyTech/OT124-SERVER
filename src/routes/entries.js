a990a5f8c8553c5b07a7fd79d3e89cba3774de01
juanpa0011 <juanpablo1996epifani@gmail.com>
2022-01-31 18:28:14 -0300

    Changing entriestoNews

--- src/routes/entries.js
+++ src/routes/entries.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const entriesController = require('../controllers/entries')
+const entriesController = require('../controllers/news')
 const {validation} = require('../middlewares/validator')
 const {entriesCreatorSchema} = require('../validations/entriesSchema')
 
