2f0d88deea51b2f513d5febf4858516e902dfc98
necogamy <madrazonecogamy@gmail.com>
2022-02-02 17:51:52 -0300

    Project requeriments done, get all users

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,9 +3,13 @@ var router = express.Router();
 const userController = require('../controllers/users');
 const {  validationIdParams } = require('../middlewares/validator');
 const { userDeleteSchema } = require('../validations/usersSchema');
+const { isAdmin } = require('../middlewares/isRole');
 
 //delete a 'contact' or user
 //,validation(userDeleteSchema)
 router.delete('/:id', userController.deleteUser );
+
+// GET of all users, only for admin users
+router.get('/', isAdmin, userController.getAllUsers);
 
 module.exports = router;
