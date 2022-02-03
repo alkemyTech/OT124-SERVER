918289fcf50e5cd5878ccecf2107801223bf074c
necogamy <madrazonecogamy@gmail.com>
2022-02-03 19:27:23 -0300

    Added validate token middleware

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,12 +4,13 @@ const userController = require('../controllers/users');
 const {  validationIdParams } = require('../middlewares/validator');
 const { userDeleteSchema } = require('../validations/usersSchema');
 const { isAdmin } = require('../middlewares/isRole');
+const { validateToken } = require('../middlewares/auth');
 
 //delete a 'contact' or user
 //,validation(userDeleteSchema)
 router.delete('/:id', userController.deleteUser );
 
 // GET of all users, only for admin users
-router.get('/', isAdmin, userController.getAllUsers);
+router.get('/', validateToken, isAdmin, userController.getAllUsers);
 
 module.exports = router;
