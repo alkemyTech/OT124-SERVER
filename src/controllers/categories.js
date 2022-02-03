f904f92d0dfd46ac2e79232978ae574c4f91eac3
Diego Sosa <dhsosa98@gmail.com>
2022-02-03 14:15:51 -0300

    refactor categories put router

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,6 +1,24 @@
 const db = require('../models')
 entity = 'categories'
 
+const updateCategory = async function(req, res, next) {
+  db[entity].findAll({
+      where: {
+        id: { [Op.like]: req.params.id },
+      },
+    }).then(function (categories) {
+      if (!categories) {
+          db[entity].update({
+              name: req.body.name,
+              description: req.body.description                
+            });      
+            return res.json("Category created successfully");
+      } else {       
+          return res.json("The category already exists");
+      }
+    });
+};
+
 const createCategory = async function (req, res, next) {
     try {
       const categoryCreated = await db[entity].create(req.body)
@@ -14,6 +32,7 @@ const createCategory = async function (req, res, next) {
     }
   };
   const categoriesController = {
-      createCategory
+      createCategory,
+      updateCategory
   }
   module.exports = categoriesController;
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,32 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const { Op } = require("sequelize");
-const { createCategory } = require('../controllers/categories');
+const { createCategory, updateCategory } = require('../controllers/categories');
 const { validateToken } = require('../middlewares/auth');
 const { isAdmin, isDev } = require('../middlewares/isRole');
 const { validation } = require('../middlewares/validator');
 const {categoriesCreatorSchema} = require('../validations/categoriesSchema')
 
 /* PUT categories. */
-router.put('/categories/:id', function(req, res, next) {
-    db.Categories.findAll({
-        where: {
-          id: { [Op.like]: req.params.id },
-        },
-      }).then(function (categories) {
-        if (!categories) {
-            db.Categories.update({
-                name: req.body.name,
-                description: req.body.description                
-              });      
-              return res.json("Category created successfully");
-          
-        } else {       
-            return res.json("The category already exists");
-        }
-      });
-
-});
+router.put('/:id', updateCategory)
 
 router.post('/', validateToken, isAdmin, validation(categoriesCreatorSchema), createCategory)
 
