8dacc64cd8f12804268d493a398d9dadd5be0431
Nicolas-Marsan <nicomar80@gmail.com>
2022-02-03 15:08:52 -0300

    create endpoint delete categorie

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const categoriesController = require("../controllers/categories");
 
 router.put('/:id', categoriesController.putCategories );
+router.delete('/:id', categoriesController.deleteCategorie );
 
 module.exports = router;
 
