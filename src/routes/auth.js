b044555685fe5749aec1313991a7553ec3f19419
Diego Sosa <dhsosa98@gmail.com>
2022-02-03 14:47:15 -0300

    updated auth login route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
-const loginController  = require("../controllers/login");
 const authController = require("../controllers/auth");
 const { validation } = require("../middlewares/validator");
 const { registerSchema ,loginSchema } = require("../validations/authSchema");
 const { matchCredentials } = require('../middlewares/auth');
 
 
-router.post('/login',validation(loginSchema), matchCredentials,  loginController.login)
+router.post('/login',validation(loginSchema), matchCredentials,  authController.login)
 router.post("/register",validation(registerSchema), authController.registerUser);
 router.get("/me", authController.getMe);
 
