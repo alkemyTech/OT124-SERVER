93c698a13d9e91b22016ca484a82fec4c9ac8181
necogamy <madrazonecogamy@gmail.com>
2022-02-04 18:33:12 -0300

    Fix

--- src/validations/contactsSchema.js
+++ src/validations/contactsSchema.js
@@ -1,10 +1,10 @@
 const yup = require("yup");
 
 const contactPostSchema = yup.object().shape({
-  name: yup.string('Name must be a string').required('Name is required'),
-  email: yup.string('Must be a string').required('Email is required').email('Invalid email format'),
-  phone: yup.phone('Must be a phone'),
-  message: yup.string('Must be a string')
+  name: yup.string().required('Name is required'),
+  email: yup.string().required('Email is required').email('Invalid email format'),
+  phone: yup.string().min(8).max(12),
+  message: yup.string()
 });
 
 module.exports = { 
