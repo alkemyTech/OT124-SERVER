2e55170406ac220bbe3cc6377dcc75f9be50bcf2
Nicolás López <36177527+necogamy@users.noreply.github.com>
2022-02-04 16:24:52 -0300

    Ot124 72 (#42)
    * Fixed: package.json && scaffold project (#1)
    
    * Fixed: package.json && scaffold project
    
    * Added model & migration of entries and organization
    
    * package-lock.json updated
    
    * Added package-lock
    
    * Task requeriments done, endpoint for save contacts
    
    Co-authored-by: Francisco Ríos Cabello <francisco.rios.cabello@gmail.com>

---
+++ src/validations/contactsSchema.js
@@ -0,0 +1,12 @@
+const yup = require("yup");
+
+const contactPostSchema = yup.object().shape({
+  name: yup.string('Name must be a string').required('Name is required'),
+  email: yup.string('Must be a string').required('Email is required').email('Invalid email format'),
+  phone: yup.phone('Must be a phone'),
+  message: yup.string('Must be a string')
+});
+
+module.exports = { 
+  contactPostSchema
+};
