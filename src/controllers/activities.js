e9a4b9c7013afe6689af59d583f2cc61d065968d
Rafael Lynch <rafalynch.dev@gmail.com>
2022-02-06 15:47:46 -0300

    Activitites PUT and POST route, schema and model

--- src/controllers/activities.js
+++ src/controllers/activities.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 const entity = "activities";
-const updateFile = require("../services/aws_s3");
+const { uploadFile, updateFile } = require("../services/aws_s3");
 
 const getActivities = async function (req, res, next) {
   try {
@@ -13,25 +13,15 @@ const getActivities = async function (req, res, next) {
 
 const postActivities = async function (req, res, next) {
   try {
-    const { name, img, content } = req.body;
-    const error = [];
+    const { url } = await uploadFile(req.file, next);
+    req.body.image = url;
 
-    if (!name) {
-      error.push({ text: "Agregar el nombre de la actividad" });
-    }
-    if (!content) {
-      error.push({ text: "Agregar una breve descripcion de la actividad" });
-    }
-    if (error.length > 0) {
-      return res.send({ title: "Activities", message: error });
-    } else {
-      const newActivity = await db[entity].create({ name, content, img });
-      res.send({
-        title: "Activities",
-        message: "Actividad creada con exito!",
-        newActivity: newActivity,
-      });
-    }
+    const newActivity = await db[entity].create(req.body);
+    res.status(201).send({
+      title: "Activities",
+      message: "New activity created",
+      newActivity: newActivity,
+    });
   } catch (err) {
     next(err);
   }
@@ -39,28 +29,50 @@ const postActivities = async function (req, res, next) {
 
 const putActivities = async function (req, res, next) {
   const { id } = req.params;
-  const { name, content } = req.body;
+  const { body } = req;
 
   try {
+    // Find activity to update
     const activityFound = await db[entity].findOne({
       where: {
         id,
       },
     });
 
-    if (name) {
-      activityFound.name = name;
-    }
-    if (req.file) {
-      await updateFile(img, req.file, next);
-    }
-    if (content) {
-      activityFound.content = content;
+    // If not found, return
+    if (!activityFound) {
+      const error = new Error("Activity not found");
+      throw error;
     }
 
+    // If a file was sent
+    if (req.file) {
+      // If also a key was provided, update file with key and and update acivityFound url
+      if (body.key) {
+        const { url } = await updateFile(body.key, req.file, next);
+        activityFound.image = url;
+        // If the is no provided key, upload new file and update acivityFound url
+      } else {
+        const { url } = await uploadFile(req.file, next);
+        activityFound.image = url;
+      }
+    }
+
+    // If name was provided, update activityfound
+    if (body.name) {
+      activityFound.name = body.name;
+    }
+    // If content was provided, update activityFound
+    if (body.content) {
+      activityFound.content = body.content;
+    }
+
+    // Save new activityFound properties in db
     await activityFound.save();
 
-    return res.json("Actividad actualizada");
+    return res.status(200).json({
+      msg: "Activity succesfully updated",
+    });
   } catch (err) {
     next(err);
   }
--- src/validations/activitiesSchema.js
+++ src/validations/activitiesSchema.js
@@ -1,11 +1,25 @@
 const yup = require("yup");
 
-const FILE_SIZE = 50000; // Bytes (5MB)
+const FILE_SIZE = 5000000; // Bytes (5MB)
 const SUPPORTED_FORMATS = ["image/jpg", "image/jpeg", "image/gif", "image/png"];
+
+const activitiesSchema = yup.object().shape({
+  name: yup.string("Name must be a string").required("Name is required"),
+  content: yup
+    .string("Content must be a string")
+    .required("content is required"),
+});
 
 const putActivitiesSchema = yup.object().shape({
   name: yup.string("Name must be a string"),
   content: yup.string("Content must be a string"),
+  key: yup
+    .string()
+    .trim()
+    .matches(
+      /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i,
+      "Key is not in correct format"
+    ),
 });
 
 const fileSchema = yup.object().shape({
@@ -24,7 +38,32 @@ const fileSchema = yup.object().shape({
       } else {
         return SUPPORTED_FORMATS.includes(value.mimetype);
       }
+    })
+    .required("File is required"),
+});
+
+const putFileSchema = yup.object().shape({
+  file: yup
+    .mixed()
+    .test("fileSize", "File Size is too large", (value) => {
+      if (value == null) {
+        return true;
+      } else {
+        return value.size <= FILE_SIZE;
+      }
+    })
+    .test("fileType", "Unsupported File Format", (value) => {
+      if (value == null) {
+        return true;
+      } else {
+        return SUPPORTED_FORMATS.includes(value.mimetype);
+      }
     }),
 });
 
-module.exports = { putActivitiesSchema, fileSchema };
+module.exports = {
+  activitiesSchema,
+  putActivitiesSchema,
+  fileSchema,
+  putFileSchema,
+};
