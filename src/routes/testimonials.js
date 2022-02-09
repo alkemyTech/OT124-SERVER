8e91bb4b6463a902c4b7d895e74b03435a1b4580
Rafael Lynch <rafalynch.dev@gmail.com>
2022-02-09 16:54:11 -0300

    Add delete testimonials route

--- src/routes/testimonials.js
+++ src/routes/testimonials.js
@@ -1,14 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const multer  = require('multer')
-const upload = multer()
+const multer = require("multer");
+const upload = multer();
 
-const testimonialsController = require('../controllers/testimonials')
-const {validation} = require('../middlewares/validator')
-const {testimonialsCreatorSchema} = require('../validations/testimonialSchema')
+const testimonialsController = require("../controllers/testimonials");
+const { validation } = require("../middlewares/validator");
+const {
+  testimonialsCreatorSchema,
+} = require("../validations/testimonialSchema");
 
 /* POST testimonials content. */
-router.post('/', upload.single('avatar'), validation(testimonialsCreatorSchema) ,testimonialsController.createTestimonial );
+router.post(
+  "/",
+  upload.single("avatar"),
+  validation(testimonialsCreatorSchema),
+  testimonialsController.createTestimonial
+);
+
+router.delete("/:id");
 
 module.exports = router;
