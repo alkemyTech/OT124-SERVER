46e2a415c854269934947ba4a313bb04357e37b8
Diego Sosa <dhsosa98@gmail.com>
2022-02-10 09:33:37 -0300

    created put testimonials route

--- src/routes/testimonials.js
+++ src/routes/testimonials.js
@@ -1,14 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const multer  = require('multer')
-const upload = multer()
+const multer = require("multer");
+const upload = multer();
 
-const testimonialsController = require('../controllers/testimonials')
-const {validation} = require('../middlewares/validator')
-const {testimonialsCreatorSchema} = require('../validations/testimonialSchema')
+const testimonialsController = require("../controllers/testimonials");
+const authController = require("../middlewares/auth");
+const { isAdmin } = require("../middlewares/isRole");
+const { validation, fileValidation } = require("../middlewares/validator");
+const { fileSchema } = require("../validations/fileSchema");
+const {
+  testimonialsCreatorSchema,
+} = require("../validations/testimonialSchema");
 
-/* POST testimonials content. */
-router.post('/', upload.single('avatar'), validation(testimonialsCreatorSchema) ,testimonialsController.createTestimonial );
+/* POST testimonials route. */
+router.post(
+  "/",
+  authController.validateToken,
+  isAdmin,
+  upload.single("avatar"),
+  validation(testimonialsCreatorSchema),
+  fileValidation(fileSchema),
+  testimonialsController.createTestimonial
+);
+/* PUT testimonials route. */
+router.put(
+  "/:id",
+  authController.validateToken,
+  isAdmin,
+  upload.single("lastimage"),
+  validation(testimonialsCreatorSchema),
+  fileValidation(fileSchema),
+  testimonialsController.updateTestimonial
+);
 
 module.exports = router;
