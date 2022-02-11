667690c21f6f9d6251b0269bf0de8c7af3cfe463
juanpa0011 <juanpablo1996epifani@gmail.com>
2022-02-11 11:27:51 -0300

    Update Members.

--- src/routes/members.js
+++ src/routes/members.js
@@ -3,6 +3,7 @@ const membersController = require("../controllers/members");
 var router = express.Router();
 const { validation } = require("../middlewares/validator");
 const { memberPostSchema } = require("../validations/membersSchema");
+const { validateToken } = require("../middlewares/auth");
 
 /* GET all members */
 router.get("/", membersController.getMembers);
@@ -11,5 +12,7 @@ router.get("/", membersController.getMembers);
 router.post("/", validation(memberPostSchema), membersController.postMember);
 
 router.delete("/:id", membersController.deleteMember);
+
+router.put("/:id", validateToken, validation(memberPostSchema), membersController.updateMember );
 
 module.exports = router;
