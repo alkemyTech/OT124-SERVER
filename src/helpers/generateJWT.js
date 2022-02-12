a9d2e22b1bfd28b7c3771090efc8db89c3637386
Diego Sosa <dhsosa98@gmail.com>
2022-02-12 06:10:24 -0300

    added user firstName and lastName to jwt

--- src/helpers/generateJWT.js
+++ src/helpers/generateJWT.js
@@ -4,9 +4,14 @@ require("dotenv").config();
 const generateJWT = async (user) => {
   const token = jwt.sign(
     {
+      user: 
+      {
       id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
       email: user.email,
       role: user.role
+      }
     },
     process.env.JWT_SECRET,
     {
