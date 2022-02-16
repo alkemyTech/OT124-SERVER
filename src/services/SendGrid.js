6727f8a20e5faf74c9555aa9715135bfc690fe20
Franco Martin <francomartin.personal@gmail.com>
2022-02-16 17:02:39 -0300

    aaa

--- src/services/SendGrid.js
+++ src/services/SendGrid.js
@@ -1,10 +1,5 @@
 require("dotenv").config();
 const sgMail = require('@sendgrid/mail')
-<<<<<<< HEAD
-require("dotenv").config();
-const KEY = process.env.SENDGRID_API_KEY;
-sgMail.setApiKey(KEY)
-=======
 const KEY = process.env.SENDGRID_API_KEY;
 sgMail.setApiKey(KEY)
 
@@ -18,7 +13,6 @@ const SendGrid = async (msg) => {
   catch (err) {
     console.log(err)
   }
->>>>>>> 8113f2d96c94952a7ce6e17b51e25a828e7ee8d5
 
 
 }
