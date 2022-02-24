3b019f17d5458fecdfc3f3c70f151e77b62c3ebb
Nicolás López <36177527+necogamy@users.noreply.github.com>
2022-02-24 17:43:59 -0300

    OT124-113 | Test User Endpoint (#82)
    * Made all the users endpoints
    
    * Finished all the users endpoint and respective tests

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,16 +1,27 @@
 var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/users');
-const {  validationIdParams } = require('../middlewares/validator');
-const { userDeleteSchema } = require('../validations/usersSchema');
+const { validation } = require('../middlewares/validator');
+const { userUpdateSchema, userCreateSchema } = require('../validations/usersSchema');
 const { isAdmin } = require('../middlewares/isRole');
 const { validateToken } = require('../middlewares/auth');
 
-//delete a 'contact' or user
-//,validation(userDeleteSchema)
-router.delete('/:id', validateToken, isAdmin, userController.deleteUser );
+// DELETE a 'contact' or user
+router.delete('/:id', validateToken, isAdmin, userController.deleteUser);
 
 // GET of all users, only for admin users
 router.get('/', validateToken, isAdmin, userController.getAllUsers);
+
+// GET a user, only for admin users
+router.get('/:id', validateToken, isAdmin, userController.getUser);
+
+// UPDATE a user, only for admin users
+router.put('/:id', validateToken, isAdmin, validation(userUpdateSchema), userController.updateUser);
+
+// POST a user, only for admin users and for testing purposes
+router.post('/', validateToken, isAdmin, validation(userCreateSchema), userController.postUser);
+
+// Restores a soft deleted user
+router.put('/restore/:id', validateToken, isAdmin, userController.restore);
 
 module.exports = router;
