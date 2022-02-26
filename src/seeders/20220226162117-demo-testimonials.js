e7aea6decfe429b2e721ff3b7f1454e3bcbdf0a7
Rafael Lynch <rafalynch.dev@gmail.com>
2022-02-26 13:30:21 -0300

    TEstimonials seeds

---
+++ src/seeders/20220226162117-demo-testimonials.js
@@ -0,0 +1,56 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface) {
+    return queryInterface.bulkInsert(
+      "testimonials",
+      [
+        {
+          name: "Carla",
+          lastimage:
+            "https://cohorte-enero-835eb560.s3.us-east-1.amazonaws.com/7cca230b-e7eb-4702-bc88-8f51488d23c8",
+          content: `
+              Carla es una medica que ayudo en muchas campañas de la ONG.
+            `,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Pedro",
+          lastimage:
+            "https://cohorte-enero-835eb560.s3.us-east-1.amazonaws.com/d2dfceb3-545f-46f4-a02a-0974409caac0",
+          content: `
+              Pedro es un emprendedor que con su empresa dona a la ONG todos los años.
+            `,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Juan",
+          lastimage:
+            "https://cohorte-enero-835eb560.s3.us-east-1.amazonaws.com/f2149525-3eef-42dd-b936-3ba21cb123c7",
+          content: `
+              Juan es un vecino del barrio.
+            `,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Marta",
+          lastimage:
+            "https://cohorte-enero-835eb560.s3.us-east-1.amazonaws.com/5a38ab4c-6528-455a-98ea-036160d7af67",
+          content: `
+              Marta es maestra y madre de del barrio.
+            `,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete("activities", null, {});
+  },
+};
