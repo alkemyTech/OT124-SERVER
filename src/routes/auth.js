bd440d8b5444ab5c88b3b9d549dd1403b04795d6
Diego Sosa <dhsosa98@gmail.com>
2022-02-28 11:07:27 -0300

    fixed auth issues

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,12 +3,12 @@ var router = express.Router();
 const authController = require("../controllers/auth");
 const { validation } = require("../middlewares/validator");
 const { registerSchema ,loginSchema } = require("../validations/authSchema");
-const { matchCredentials } = require('../middlewares/auth');
+const { matchCredentials, validateToken } = require('../middlewares/auth');
 
 
 router.post('/login', validation(loginSchema), matchCredentials,  authController.login)
 router.post('/googleAuth', authController.googleAuth)
 router.post("/register",validation(registerSchema), authController.registerUser);
-router.get("/me", authController.getMe);
+router.get("/me", validateToken, authController.getMe);
 
 module.exports = router;
