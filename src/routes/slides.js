8000b7e2ab5e42393d805fbc16f375ac38d893db
Diego Sosa <dhsosa98@gmail.com>
2022-03-01 18:23:38 -0300

    created all routes and slides controllers

--- src/routes/slides.js
+++ src/routes/slides.js
@@ -1,26 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const slidesController = require('../controllers/slides');
-const { isAdmin } = require('../middlewares/isRole');
+const slidesController = require("../controllers/slides");
+const { isAdmin } = require("../middlewares/isRole");
 const { validation, fileValidation } = require("../middlewares/validator");
 const authController = require("../middlewares/auth");
 const { fileSchema } = require("../validations/fileSchema");
-const { updateSlideSchema } = require('../validations/slidesSchema');
+const { updateSlideSchema } = require("../validations/slidesSchema");
 
 const multer = require("multer");
 const upload = multer();
 
 // GET
-router.get('/', slidesController.getSlides);
+router.get("/", slidesController.getSlides);
+
+// GET BY ID
+router.get("/:id", slidesController.getSlideById);
+
+// DELETE
+router.delete(
+  "/:id",
+  authController.validateToken,
+  isAdmin,
+  slidesController.deleteSlide
+);
+
+//POST
+router.post(
+  "/",
+  authController.validateToken,
+  isAdmin,
+  upload.single("image"),
+  validation(updateSlideSchema),
+  fileValidation(fileSchema),
+  slidesController.createSlide
+);
 
 // PUT
-router.put('/:id', 
-    authController.validateToken,
-    isAdmin,
-    upload.single("image"),
-    validation(updateSlideSchema),
-    fileValidation(fileSchema),
-    slidesController.updateSlides
+router.put(
+  "/:id",
+  authController.validateToken,
+  isAdmin,
+  upload.single("image"),
+  validation(updateSlideSchema),
+  fileValidation(fileSchema),
+  slidesController.updateSlides
 );
 
 module.exports = router;
